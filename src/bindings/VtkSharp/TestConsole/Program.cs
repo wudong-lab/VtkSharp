2bc2055a9855309fb7d8fa715af16101a3984b11
wudong <wdhust@qq.com>
2026-06-23 00:32:11 +0800

    添加测试程序

---
+++ src/bindings/VtkSharp/TestConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using VtkSharp;
+
+namespace TestConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using var cone = vtkConeSource.New();
+
+        using var mapper = vtkPolyDataMapper.New();
+        mapper.SetInputConnection(cone.GetOutputPort());
+
+        using var actor = vtkActor.New();
+        actor.SetMapper(mapper);
+
+        using var renderer = vtkRenderer.New();
+        renderer.AddActor(actor);
+
+        using var window = vtkRenderWindow.New();
+        window.AddRenderer(renderer);
+        window.SetSize(800, 600);
+
+        using var interactor = vtkRenderWindowInteractor.New();
+        interactor.SetRenderWindow(window);
+
+        window.Render();
+        interactor.Start();
+
+        Console.WriteLine("Done!");
+    }
+}
