9ee275894ca2318a60e6b35f3737f35ab978f7df
wudong <wdhust@qq.com>
2026-06-25 19:26:27 +0800

    添加VtkSharpColor3d

---
+++ src/bindings/VtkSharp/Core/VtkSharpColor3d.cs
@@ -0,0 +1,18 @@
+﻿namespace VtkSharp;
+
+/// <summary>
+/// VTK颜色值，对应<see cref="vtkColor3d"/>
+/// </summary>
+public readonly struct VtkSharpColor3d
+{
+    public VtkSharpColor3d(double r, double g, double b)
+    {
+        this.R = r;
+        this.G = g;
+        this.B = b;
+    }
+
+    public double R { get; }
+    public double G { get; }
+    public double B { get; }
+}
