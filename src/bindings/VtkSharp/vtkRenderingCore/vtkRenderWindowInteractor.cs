37faeaaa2a05f96a020705236bc2489102dd192b
wudong <wdhust@qq.com>
2026-07-01 22:10:11 +0800

    调整代码布局

--- src/bindings/VtkSharp/vtkRenderingCore/vtkRenderWindowInteractor.cs
+++ src/bindings/VtkSharp/vtkRenderingCore/vtkRenderWindowInteractor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace VtkSharp;
 
@@ -113,4 +114,56 @@ public unsafe partial class vtkRenderWindowInteractor
     private bool IsControlKeyPressed() => this.GetControlKey() != 0;
     private bool IsShiftKeyPressed() => this.GetShiftKey() != 0;
     private bool IsAltKeyPressed() => this.GetAltKey() != 0;
+
+    public const int OneShotTimer = 1;
+    public const int RepeatingTimer = 2;
+
+    public void SetAltKey(bool altKey)
+    {
+        this.SetAltKey(altKey ? 1 : 0);
+    }
+
+    public void SetEventInformationFlipY(
+        int x,
+        int y,
+        bool controlKey,
+        bool shiftKey,
+        char keyCode = '\0',
+        int repeatCount = 0,
+        string? keySym = null)
+    {
+        this.SetEventInformationFlipY(
+            x,
+            y,
+            controlKey ? 1 : 0,
+            shiftKey ? 1 : 0,
+            keyCode,
+            repeatCount,
+            keySym ?? string.Empty);
+    }
+
+    public void SetKeyEventInformation(
+        bool controlKey,
+        bool shiftKey,
+        char keyCode,
+        int repeatCount = 0,
+        string? keySym = null)
+    {
+        this.SetKeyEventInformation(
+            controlKey ? 1 : 0,
+            shiftKey ? 1 : 0,
+            keyCode,
+            repeatCount,
+            keySym ?? string.Empty);
+    }
+
+    public void InvokeTimerEvent(int timerId)
+    {
+        vtkRenderWindowInteractor_InvokeTimerEvent(this.NativePointer, timerId);
+    }
+
+    #region Interop
+    [DllImport(InteropInfo.NativeLibraryName)]
+    private static extern void vtkRenderWindowInteractor_InvokeTimerEvent(nint self, int timerId);
+    #endregion
 }
