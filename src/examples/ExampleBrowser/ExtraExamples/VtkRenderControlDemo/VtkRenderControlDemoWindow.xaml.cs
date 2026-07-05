ef60d7c6871e7f3b908dea8324bcb21c18e105d0
wudong <wdhust@qq.com>
2026-07-05 20:55:29 +0800

    调整示例

--- src/examples/ExampleBrowser/ExtraExamples/VtkRenderControlDemo/VtkRenderControlDemoWindow.xaml.cs
+++ src/examples/ExampleBrowser/ExtraExamples/VtkRenderControlDemo/VtkRenderControlDemoWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using VtkSharp.Wpf;
 
 namespace VtkSharp.ExampleBrowser.ExtraExamples.VtkRenderControlDemo;
@@ -10,7 +11,14 @@ public partial class VtkRenderControlDemoWindow : Window
         this.InitializeComponent();
     }
 
-    private void VtkRenderControl_OnVtkRenderInitialized(object? sender, VtkRenderInitializedEventArgs e)
+    private void VtkRenderControl1_OnVtkRenderInitialized(object? sender, VtkRenderInitializedEventArgs e) { }
+
+    private void VtkRenderControl1_OnVtkRenderFailed(object? sender, VtkRenderFailedEventArgs e)
+    {
+        Debug.WriteLine($"VtkRenderControl1 failed: {e.Message}");
+    }
+
+    private void ButtonRender1_OnClick(object sender, RoutedEventArgs e)
     {
         using var cone = vtkConeSource.New();
 
@@ -19,15 +27,37 @@ public partial class VtkRenderControlDemoWindow : Window
 
         using var actor = vtkActor.New();
         actor.SetMapper(mapper);
-        actor.GetProperty().SetColor(VtkColor3d.Red);
+        actor.GetProperty().SetColor(VtkColor3d.LimeGreen);
 
-        var renderer = this.VtkRenderControl.Renderer!;
-        renderer.SetBackground(VtkColor3d.Yellow);
+        var renderer = this.VtkRenderControl1.Renderer!;
         renderer.AddActor(actor);
         renderer.ResetCamera();
 
-        this.VtkRenderControl.Render();
+        this.VtkRenderControl1.Render();
     }
 
-    private void ButtonRender_OnClick(object sender, RoutedEventArgs e) { }
+    private void VtkRenderControl2_OnVtkRenderInitialized(object? sender, VtkRenderInitializedEventArgs e) { }
+
+    private void VtkRenderControl2_OnVtkRenderFailed(object? sender, VtkRenderFailedEventArgs e)
+    {
+        Debug.WriteLine($"VtkRenderControl2 failed: {e.Message}");
+    }
+
+    private void ButtonRender2_OnClick(object sender, RoutedEventArgs e)
+    {
+        using var cone = vtkConeSource.New();
+
+        using var mapper = vtkPolyDataMapper.New();
+        mapper.SetInputConnection(cone.GetOutputPort());
+
+        using var actor = vtkActor.New();
+        actor.SetMapper(mapper);
+        actor.GetProperty().SetColor(VtkColor3d.LimeGreen);
+
+        var renderer = this.VtkRenderControl2.Renderer!;
+        renderer.AddActor(actor);
+        renderer.ResetCamera();
+
+        this.VtkRenderControl2.Render();
+    }
 }
